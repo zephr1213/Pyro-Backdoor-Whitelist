4d3baadda809af4865c7e8a8658929e277bc90e2
zephr1213 <56725817+zephr1213@users.noreply.github.com>
2021-11-07 07:20:40 -0600

    Update god.lua

--- god.lua
+++ god.lua
@@ -1,15 +1,19 @@
-local LocalPlayer = game:GetService("Players").LocalPlayer
- 
-local function Invincibility()
-if LocalPlayer.Character then
-for i, v in pairs(LocalPlayer.Character:GetChildren()) do
-if v.Name == "hitbox" then
-v:Destroy()
-end
-end
-end
-end
- 
-while wait(0.5) do
-Invincibility(LocalPlayer)
-end
+	local speaker = Players.LocalPlayer
+ local Cam = workspace.CurrentCamera
+	local Pos, Char = Cam.CFrame, speaker.Character
+	local Human = Char and Char.FindFirstChildWhichIsA(Char, "Humanoid")
+	local nHuman = Human.Clone(Human)
+	nHuman.Parent, speaker.Character = Char, nil
+	nHuman.SetStateEnabled(nHuman, 15, false)
+	nHuman.SetStateEnabled(nHuman, 1, false)
+	nHuman.SetStateEnabled(nHuman, 0, false)
+	nHuman.BreakJointsOnDeath, Human = true, Human.Destroy(Human)
+	speaker.Character, Cam.CameraSubject, Cam.CFrame = Char, nHuman, wait() and Pos
+	nHuman.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+	local Script = Char.FindFirstChild(Char, "Animate")
+	if Script then
+		Script.Disabled = true
+		wait()
+		Script.Disabled = false
+	end
+	nHuman.Health = nHuman.MaxHealth
